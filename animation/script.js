139c09557b0253bddb01adb6b035514469472a0d
Saya <rahymgalievasaya@gmail.com>
2022-03-10 20:26:32 +0600

    setTimeout and setInterval

---
+++ animation/script.js
@@ -0,0 +1,20 @@
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', myAnimation);
+
+function myAnimation() {
+    const elem = document.querySelector('.box');
+    let pos = 0;
+
+    const id = setInterval(frame, 10);
+
+    function frame(){
+        if(pos == 300) {
+            clearInterval();
+        } else {
+            pos++;
+            elem.style.top = pos + "px";
+            elem.style.left = pos + "px";
+        }
+    }
+}
