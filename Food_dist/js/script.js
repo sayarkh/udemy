5537bd11a9a10b3767925d795645f8e53c5aec00
Saya <rahymgalievasaya@gmail.com>
2022-03-11 17:19:44 +0600

    modal window added

--- Food_dist/js/script.js
+++ Food_dist/js/script.js
@@ -88,5 +88,50 @@ window.addEventListener('DOMContentLoaded', () => {
 
     setClock('.timer', deadline);
 
+    //modal
+    const btns = document.querySelectorAll('[data-modal]'),
+          close = document.querySelector('[data-close]'),
+          modal = document.querySelector('.modal');
+    
+    function openModal() {
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow = 'hidden'; 
+        clearInterval(modalTimerId);
+    }
+    
+    btns.forEach(btn => {
+        btn.addEventListener('click', openModal);
+    });
+
+    function closeModal() {
+        modal.classList.remove('show');
+        modal.classList.add('hide');
+        document.body.style.overflow = ''; 
+    }
+
+    close.addEventListener('click', closeModal);
+
+    modal.addEventListener('click', (e) =>{
+        if(e.target == modal) {
+            closeModal();
+        }
+    });
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code == "Escape" && modal.classList.contains('show')) {
+            closeModal();
+        }
+    });
+
+    const modalTimerId = setTimeout(openModal, 3000);
+
+    function showModalByScroll() {
+        if(window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll);
+        }
+    }
+    window.addEventListener('scroll', showModalByScroll);
 });
 
